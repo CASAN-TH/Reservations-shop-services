4e8c5f537aad34422f6dc3e3781a23c30388ef80
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-01-25 11:47:16 +0700

    [policy open]

--- src/modules/shop/routes/route.js
+++ src/modules/shop/routes/route.js
@@ -4,11 +4,11 @@ var controller = require('../controllers/controller'),
 module.exports = function (app) {
     var url = '/api/shops';
     var urlWithParam = '/api/shops/:shopId';
-    app.route(url)//.all(policy.isAllowed)
+    app.route(url).all(policy.isAllowed)
         .get(controller.getList)
         .post(controller.create);
 
-    app.route(urlWithParam)//.all(policy.isAllowed)
+    app.route(urlWithParam).all(policy.isAllowed)
         .get(controller.read)
         .put(controller.update)
         .delete(controller.delete);
